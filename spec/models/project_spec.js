607af1895be8e9ad4a879e1d2fabdfaa0e2b70a8
Luiz Fernando Signorelli <luiz.sg@gmail.com>
2011-04-14 01:58:16 -0300

    implemented projects with more than one team

--- spec/models/project_spec.js
+++ spec/models/project_spec.js
@@ -1,7 +1,7 @@
 Screw.Unit(function() {
     describe('Project', function() {
         var project
-        var team1
+        var team1, team2
         before(function() {
             team1 =  new Team({ people: 4,
                 initial_productivity: 0.7,
@@ -9,13 +9,18 @@ Screw.Unit(function() {
                 productivity_increment: 0.1,
                 initial_sprint: 1
             })
+            team2 =  new Team({ people: 4,
+                initial_productivity: 0.5,
+                max_productivity: 0.8,
+                productivity_increment: 0.05,
+                initial_sprint: 3
+            })
         });
         describe('#generate sprints with 1 team', function() {
             before(function() {
                 project = new Project({ sprint_length: 10,
                     teams:[team1] })
             });
-
             it("generates 1 sprint", function() {
                 project.generate_sprints(1)
                 expect(project.sprints()).to(have_length, 1)
@@ -30,7 +35,7 @@ Screw.Unit(function() {
                 expect(sprint2.points()).to(equal, 32)
                 expect(sprint2.working_days()).to(equal,12)
             });
-            it("respect the max productivity", function() {
+            it("respects the max productivity", function() {
                 project.generate_sprints(3)
                 expect(project.sprints()).to(have_length, 3)
                 var sprint3 = project.sprints()[2]
@@ -38,5 +43,23 @@ Screw.Unit(function() {
                 expect(sprint3.working_days()).to(equal,12)
             });
         });
+
+        describe('#generate sprints with more than 1 team', function() {
+            before(function() {
+                project = new Project({ sprint_length: 10,
+                    teams:[team1, team2] })
+            });
+            it("respects the team initial sprint", function() {
+                project.generate_sprints(3)
+                expect(project.sprint(1).teams()).to(have_length,1)
+                expect(project.sprint(2).teams()).to(have_length,1)
+                expect(project.sprint(3).teams()).to(have_length,2)
+            });
+            it("considers the cost of team integration", function() {
+                project.generate_sprints(3)
+                expect(project.sprint(3).working_days()).to(equal, 17)
+            });
+        });
+
     });
 });
